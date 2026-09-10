d1515f912fd542c008d97d5d686f451adb6858b4
Anish Bade <anishbade@Mac.fritz.box>
2026-09-10 12:40:59 +0200

    Updates

---
+++ precache-manifest.244ea5869f986a5729d8e22660b343f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd70b7dfdf7be41c092fd9a7fadd42a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cebbf03cdad7ff7990ef",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "cd29197c32c8baf42a2b",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "cebbf03cdad7ff7990ef",
+    "url": "/static/js/2.ded3c058.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.ded3c058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd29197c32c8baf42a2b",
+    "url": "/static/js/main.66baa2e6.chunk.js"
+  },
+  {
+    "revision": "28c2c22babe18a4a7e18",
+    "url": "/static/js/runtime-main.a490ef8a.js"
+  }
+]);
